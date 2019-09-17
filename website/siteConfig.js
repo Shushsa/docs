de89ce7b71613d786d5a94091d6d6aabc02b356e
Knight <43088213+AmanuelYosief@users.noreply.github.com>
2019-09-17 15:33:33 +0100

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,7 +72,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/Kira-Exchange/docs',
+  // repoUrl: 'https://github.com/Kira-Exchange/docs',
 };
 
 module.exports = siteConfig;
